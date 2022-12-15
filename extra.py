56645062cc48a7d11a446351137db0e2db40d6dd
Krzysztof Kurowski <kkur@devs.local>
2022-12-15 16:39:06 +0100

    Fillcolor added

--- extra.py
+++ extra.py
@@ -2,17 +2,18 @@ from turtle import *
 import math
 def square(size,linecolor,fillcol,coord_x,coord_y,angle):
     color(linecolor)
-    fillcolor(fillcol)
     up()
     setx(coord_x)
     sety(coord_y)
     down()
+    fillcolor(fillcol)
+    begin_fill()
     right(angle)
     for i in range (4):
         forward(size)
         right(90)
+    end_fill()
 
-square(200,"red","black",-600,400,0)
 
 def right_triangle(size,linecolor,fillcol,coord_x,coord_y,angle):
     color(linecolor)
@@ -21,6 +22,7 @@ def right_triangle(size,linecolor,fillcol,coord_x,coord_y,angle):
     setx(coord_x)
     sety(coord_y)
     down()
+    begin_fill()
     right(angle)
     right(180)
     forward(size/2)
@@ -28,9 +30,8 @@ def right_triangle(size,linecolor,fillcol,coord_x,coord_y,angle):
     forward((size*math.sqrt(3))/2)
     right(150)
     forward(size)
+    end_fill()
     
-    
-right_triangle(300,"red","black",-200,100,0)
 
 def trefoil(size,linecolor,fillcol,coord_x,coord_y,angle):
     color(linecolor)
@@ -39,12 +40,14 @@ def trefoil(size,linecolor,fillcol,coord_x,coord_y,angle):
     setx(coord_x)
     sety(coord_y)
     down()
+    begin_fill()
     right(angle)
     for i in range(3):
         circle(size,240)
         right(120)
+    right(30)
+    end_fill()
 
-trefoil(50,"red","black",0,-200,0)
 
 def cross(size,linecolor,fillcol,coord_x,coord_y,angle):
     color(linecolor)
@@ -53,6 +56,7 @@ def cross(size,linecolor,fillcol,coord_x,coord_y,angle):
     setx(coord_x)
     sety(coord_y)
     down()
+    begin_fill()
     right(angle)
     for i in range(4):
         forward(size)
@@ -61,7 +65,7 @@ def cross(size,linecolor,fillcol,coord_x,coord_y,angle):
         right(90)
         forward(size)
         left(90)
-cross(80,"red","black",-200,-300,0)
+    end_fill()
 
 def rhombus(size,linecolor,fillcol,coord_x,coord_y,angle):
     color(linecolor)
@@ -70,12 +74,24 @@ def rhombus(size,linecolor,fillcol,coord_x,coord_y,angle):
     setx(coord_x)
     sety(coord_y)
     down()
+    begin_fill()
+    right(30)
     right(angle)
-    right(90)
-    for i in range (4):
-        forward(size)
-        right(90)
+    for i in range (1,5):
+        if i%2!=0:
+            forward(size)
+            right(120)
+        else:
+            forward(size)
+            right(60)
+    end_fill()
+
+square(200,"red","black",-600,400,0)
+right_triangle(300,"red","black",-200,100,0)
+trefoil(50,"red","black",0,-200,0)
+cross(80,"red","black",-200,-300,0)
 rhombus(80,"red","black",400,-200,0)
+
 done()
     
         
